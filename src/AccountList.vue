3ad48e16116b6798a992c65b0c17f34d84728d0f
Brett Henderson <brett@scrat.local>
2017-06-24 20:02:36 +1000

    mproving the layout with Vuetify

--- src/AccountList.vue
+++ src/AccountList.vue
@@ -9,19 +9,24 @@
 	        </v-toolbar>
 	
 			<v-list two-line>
-				<template v-for="item in accounts">
-					<v-list-item v-bind:key="item.id">
-						<v-list-tile-content>
-							<v-list-tile-title v-html="item.name"></v-list-tile-title>
-							<v-list-tile-sub-title v-html="item.balance"></v-list-tile-sub-title>
-						</v-list-tile-content>
+				<template v-for="account in accounts">
+					<v-list-item v-bind:key="account.id">
+						<v-list-tile account>
+							<v-list-tile-content>
+								<v-list-tile-title v-html="account.name"></v-list-tile-title>
+								<v-list-tile-sub-title v-html="account.balance"></v-list-tile-sub-title>
+							</v-list-tile-content>
+	
+						</v-list-tile>
 					</v-list-item>
 				</template>
 				<v-list-item v-bind:key="networth">
-					<v-list-tile-content>
-						<v-list-tile-title>NetWorth</v-list-tile-title>
-						<v-list-tile-sub-title v-html="networth"></v-list-tile-sub-title>
-					</v-list-tile-content>
+					<v-list-tile networth>
+						<v-list-tile-content>
+							<v-list-tile-title>NetWorth</v-list-tile-title>
+							<v-list-tile-sub-title v-html="networth"></v-list-tile-sub-title>
+						</v-list-tile-content>
+					</v-list-tile>
 				</v-list-item>
 			</v-list>
 	</v-card>
