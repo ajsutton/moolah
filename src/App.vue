db48ea445cece80665319e940e57eb4e5c7a499e
Adrian Sutton <adrian@symphonious.net>
2017-06-30 05:16:15 +1000

    Check whether user is logged in and update UI accordingly.

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,13 @@
     </v-toolbar>
     <main>
       <v-container fluid>
-        <router-view></router-view>
+        <hello v-if="!loggedIn"></hello>
+        <router-view v-if="loggedIn"></router-view>
       </v-container>
     </main>
     <v-footer>
-      <div>Hero image by <a class="white--text" href="https://www.flickr.com/photos/tfpc/8626221286/in/photolist-e9gD4d-quxKNs-ojD7vn-oLTTjf-bhu5VR-ooVMzE-stE5Hy-UHvTs5-cjKs7Q-qtZFwc-on9mWD-fy7Wfq-974AH9-nZYchC-nqxyve-d4cqn1-Vot94w-oqAd7p-reacHf-pN74aF-e9X8Gr-roWWAp-Vtb3iX-q13JJE-8j3xrm-qc4tA4-mxPSUi-omprtS-i4EEEk-rKzLGe-TQTbpq-ejkdJi-mfQ4eZ-cAivyE-n2EDm-p1Qcqo-gNM15i-S8jsyY-jvyQyz-gYBgNG-prbC1E-hgpcdc-fMWVLT-fwKmbh-fdoGLG-NZiXrx-bQHfND-ouwqpR-cvAj4Y-iHVNN5">Toby Charlton-Taylor</a>.</div>
+      <div v-if="!loggedIn">Hero image by
+        <a class="white--text" href="https://www.flickr.com/photos/tfpc/8626221286/in/photolist-e9gD4d-quxKNs-ojD7vn-oLTTjf-bhu5VR-ooVMzE-stE5Hy-UHvTs5-cjKs7Q-qtZFwc-on9mWD-fy7Wfq-974AH9-nZYchC-nqxyve-d4cqn1-Vot94w-oqAd7p-reacHf-pN74aF-e9X8Gr-roWWAp-Vtb3iX-q13JJE-8j3xrm-qc4tA4-mxPSUi-omprtS-i4EEEk-rKzLGe-TQTbpq-ejkdJi-mfQ4eZ-cAivyE-n2EDm-p1Qcqo-gNM15i-S8jsyY-jvyQyz-gYBgNG-prbC1E-hgpcdc-fMWVLT-fwKmbh-fdoGLG-NZiXrx-bQHfND-ouwqpR-cvAj4Y-iHVNN5">Toby Charlton-Taylor</a>.</div>
       <v-spacer></v-spacer>
       <div>© {{ new Date().getFullYear() }}</div>
     </v-footer>
@@ -22,20 +24,35 @@
 </template>
 
 <script>
-  import AccountList from "./AccountList";
-  import LoginPanel from "./LoginPanel";
-  export default {
-    name: 'app',
-    data() {
-      return {
-        loggedIn: false
+import AccountList from './AccountList';
+import LoginPanel from './LoginPanel';
+import Hello from './components/Hello';
+import client from './api/client';
+export default {
+  name: 'app',
+  data() {
+    return {
+      loggedIn: false,
+      userId: undefined,
+      profile: {
+        givenName: undefined,
+        familyName: undefined,
       }
-    },
-    components: {
-      AccountList,
-      LoginPanel,
     }
+  },
+  components: {
+    AccountList,
+    LoginPanel,
+    Hello,
+  },
+  async created() {
+    const state = await client.userProfile();
+    console.log(state);
+    this.loggedIn = state.loggedIn;
+    this.userId = state.userId;
+    this.profile = state.profile;
   }
+}
 </script>
 
 <style lang="stylus">
--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,17 @@
+function options(options = {}) {
+	return Object.assign({}, {
+		credentials: 'include',
+	}, options);
+}
+
 export default {
-	async accounts () {
-	  return fetch('/api/accounts/')
+	async accounts() {
+		return fetch('/api/accounts/', options())
 	    .then(response => response.json());
-	}
+	},
+
+	async userProfile() {
+		return fetch('/api/auth/', options())
+			.then(response => response.json());
+	},
 }
