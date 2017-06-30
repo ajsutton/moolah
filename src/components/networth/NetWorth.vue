4580c279c1a89da786255a87bd867c9d601fb765
Adrian Sutton <adrian@symphonious.net>
2017-06-30 20:12:08 +1000

    Display something slightly better than  a not found page.

---
+++ src/components/networth/NetWorth.vue
@@ -0,0 +1,6 @@
+<template>
+  <v-layout column>
+    <h2>Analysis</h2>
+    <p>Let's pretend there's some really clever analysis here.</p>
+  </v-layout>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import NotFound from '@/components/NotFound';
+import NetWorth from '@/components/networth/NetWorth';
 
 Vue.use(Router);
 
 export default new Router({
   mode: 'history',
   routes: [
+    {path: '/', component: NetWorth},
     {path: '*', component: NotFound},
   ],
 });
