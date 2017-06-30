df62e304f9425e2724a7254e95142584ce7f473e
Adrian Sutton <suttona@lmax.com>
2017-07-01 09:36:07 +1000

    Introduce a reusable component for formatting money amounts.

--- src/components/AccountList.vue
+++ src/components/AccountList.vue
@@ -1,72 +1,71 @@
 <template>
-  <v-list class="pt-0" dense>
-    <v-divider></v-divider>
-    <v-list-item v-for="account in accounts" :key="account.id">
-      <v-list-tile avatar ripple :href="accountLink(account)" :router="true">
-        <v-list-tile-action>
-          <v-icon light>{{ icon(account.type) }}</v-icon>
-        </v-list-tile-action>
-        <v-list-tile-content>
-          <v-list-tile-title>{{ account.name }}</v-list-tile-title>
-        </v-list-tile-content>
-        <v-list-tile-action>
-          <v-chip>{{account.balance | money}}</v-chip>
-        </v-list-tile-action>
-      </v-list-tile>
-    </v-list-item>
-
-    <v-list-item>
-      <v-list-tile avatar ripple href="/" :router="true">
-        <v-list-tile-action>
-          <v-icon light>trending_up</v-icon>
-        </v-list-tile-action>
-        <v-list-tile-content>
-          <v-list-tile-title>Net Worth</v-list-tile-title>
-        </v-list-tile-content>
-        <v-list-tile-action>
-          <v-chip>{{networth | money}}</v-chip>
-        </v-list-tile-action>
-      </v-list-tile>
-    </v-list-item>
-  </v-list>
+    <v-list class="pt-0" dense>
+        <v-divider></v-divider>
+        <v-list-item v-for="account in accounts" :key="account.id">
+            <v-list-tile avatar ripple :href="accountLink(account)" :router="true">
+                <v-list-tile-action>
+                    <v-icon light>{{ icon(account.type) }}</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                    <v-list-tile-title>{{ account.name }}</v-list-tile-title>
+                </v-list-tile-content>
+                <v-list-tile-action>
+                    <monetary-amount :value="account.balance"></monetary-amount>
+                </v-list-tile-action>
+            </v-list-tile>
+        </v-list-item>
+        <v-divider></v-divider>
+        <v-list-item>
+            <v-list-tile avatar ripple href="/" :router="true">
+                <v-list-tile-action>
+                    <v-icon light>trending_up</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                    <v-list-tile-title>Net Worth</v-list-tile-title>
+                </v-list-tile-content>
+                <v-list-tile-action>
+                    <monetary-amount :value="networth"></monetary-amount>
+                </v-list-tile-action>
+            </v-list-tile>
+        </v-list-item>
+    </v-list>
 </template>
 
 <script>
-  import client from "@/api/client";
-  export default {
-    name: "AccountList",
-    data () {
-      return {
-        accounts: []
-      }
-    },
-    computed: {
-      networth () {
-        return this.accounts.reduce((networth, account) => networth + account.balance, 0);
-      }
-    },
-    methods: {
-      icon(type) {
-        switch (type) {
-          case 'cc':
-            return 'credit_card';
-          case 'asset':
-            return 'home';
-          default:
-            return 'account_balance';
+    import client from '@/api/client';
+    import MonetaryAmount from '@/components/util/MonetaryAmount';
+    export default {
+        name: "AccountList",
+        data () {
+            return {
+                accounts: []
+            }
+        },
+        computed: {
+            networth () {
+                return this.accounts.reduce((networth, account) => networth + account.balance, 0);
+            }
+        },
+        methods: {
+            icon(type) {
+                switch (type) {
+                    case 'cc':
+                        return 'credit_card';
+                    case 'asset':
+                        return 'home';
+                    default:
+                        return 'account_balance';
+                }
+            },
+            accountLink(account) {
+                return `/account/${encodeURIComponent(account.id)}/`;
+            }
+        },
+        created () {
+            client.accounts().then(response => this.accounts = response.accounts)
+        },
+        components: {
+            MonetaryAmount
         }
-      },
-      accountLink(account) {
-        return `/account/${encodeURIComponent(account.id)}/`;
-      }
-    },
-    filters: {
-      money(value) {
-        return '$' + (value / 100).toFixed(2);
-      }
-    },
-    created () {
-      client.accounts().then(response => this.accounts = response.accounts)
     }
-  }
 </script>
---
+++ src/components/util/MonetaryAmount.vue
@@ -0,0 +1,17 @@
+<template>
+    <span :class="colour">{{amount}}</span>
+</template>
+
+<script>
+    export default {
+        props: ['value'],
+        computed: {
+            colour() {
+                return this.value >= 0 ? 'green--text' : 'red--text';
+            },
+            amount() {
+                return '$' + (this.value / 100).toFixed(2);
+            }
+        },
+    }
+</script>
