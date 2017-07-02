46b999d175ff958a76757dd659faf4695e0a0251
Adrian Sutton <suttona@lmax.com>
2017-07-02 19:44:05 +1000

    Add a method to client to retrieve all transactions for an account. Not currently used but ready for someone to build a transaction list view...

--- src/api/client.js
+++ src/api/client.js
@@ -1,38 +1,42 @@
 function makeOptions(options = {}) {
-  return Object.assign({}, {
-    credentials: 'include',
-  }, options);
+    return Object.assign({}, {
+        credentials: 'include',
+    }, options);
 }
 
 async function json(url, options = {}) {
-  const response = await fetch(url, makeOptions(options));
-  if (response.ok) {
-    return response.json();
-  } else {
-    let errorResponse;
-    try {
-      errorResponse = await response.json();
-    } catch (error) {
-      errorResponse = 'An unknown error occurred.';
+    const response = await fetch(url, makeOptions(options));
+    if (response.ok) {
+        return response.json();
+    } else {
+        let errorResponse;
+        try {
+            errorResponse = await response.json();
+        } catch (error) {
+            errorResponse = 'An unknown error occurred.';
+        }
+        throw errorResponse;
     }
-    throw errorResponse;
-  }
 }
 
 export default {
-  async accounts() {
-    return json('/api/accounts/');
-  },
+    async accounts() {
+        return json('/api/accounts/');
+    },
 
-  async createAccount(account) {
-    return json('/api/accounts/', {method: 'POST', body: JSON.stringify(account)});
-  },
+    async createAccount(account) {
+        return json('/api/accounts/', {method: 'POST', body: JSON.stringify(account)});
+    },
 
-  async userProfile() {
-    return json('/api/auth/');
-  },
+    async transactions(account) {
+        return json(`/api/transactions/?account=${encodeURIComponent(account.id)}`)
+    },
 
-  async logout() {
-    return json('/api/auth/', {method: 'DELETE'});
-  },
+    async userProfile() {
+        return json('/api/auth/');
+    },
+
+    async logout() {
+        return json('/api/auth/', {method: 'DELETE'});
+    },
 };
