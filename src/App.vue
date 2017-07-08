cba447e0e5969f51ad41be9e50e1715efa894f17
Adrian Sutton <suttona@lmax.com>
2017-07-09 07:46:39 +1000

    Use a right-hand navigation drawer for the edit transaction panel instead of attempting to re-invent it.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <v-app light>
-        <v-navigation-drawer persistent clipped v-model="showMainNav" v-if="loggedIn" dark>
+        <v-navigation-drawer persistent clipped enable-resize-watcher v-model="showMainNav" v-if="loggedIn" dark>
             <v-list class="pa-0">
                 <v-list-tile avatar>
                     <v-list-tile-avatar v-if="profile.picture">
@@ -15,6 +15,9 @@
                 </v-list-tile>
             </v-list>
             <account-list></account-list>
+        </v-navigation-drawer>
+        <v-navigation-drawer persistent v-model="selectedTransaction" light right enable-resize-watcher overflow clipped>
+            <edit-transaction></edit-transaction>
         </v-navigation-drawer>
         <v-toolbar class="primary">
             <v-toolbar-side-icon dark v-if="loggedIn"
@@ -31,20 +34,23 @@
             <v-container fluid v-if="loggedIn">
                 <router-view></router-view>
             </v-container>
-            <v-footer class="primary" v-if="!loggedIn">
-                <div v-if="!loggedIn">Hero image by
-                    <a class="white--text"
-                       href="https://www.flickr.com/photos/tfpc/8626221286/in/photolist-e9gD4d-quxKNs-ojD7vn-oLTTjf-bhu5VR-ooVMzE-stE5Hy-UHvTs5-cjKs7Q-qtZFwc-on9mWD-fy7Wfq-974AH9-nZYchC-nqxyve-d4cqn1-Vot94w-oqAd7p-reacHf-pN74aF-e9X8Gr-roWWAp-Vtb3iX-q13JJE-8j3xrm-qc4tA4-mxPSUi-omprtS-i4EEEk-rKzLGe-TQTbpq-ejkdJi-mfQ4eZ-cAivyE-n2EDm-p1Qcqo-gNM15i-S8jsyY-jvyQyz-gYBgNG-prbC1E-hgpcdc-fMWVLT-fwKmbh-fdoGLG-NZiXrx-bQHfND-ouwqpR-cvAj4Y-iHVNN5">Toby Charlton-Taylor</a>.
-                </div>
-                <v-spacer></v-spacer>
-                <div><a class="white--text" href="https://moolah.rocks/">Moolah.rocks</a></div>
-            </v-footer>
         </main>
+        <v-footer class="primary" v-if="!loggedIn">
+            <div v-if="!loggedIn">Hero image by
+                <a class="white--text"
+                   href="https://www.flickr.com/photos/tfpc/8626221286/in/photolist-e9gD4d-quxKNs-ojD7vn-oLTTjf-bhu5VR-ooVMzE-stE5Hy-UHvTs5-cjKs7Q-qtZFwc-on9mWD-fy7Wfq-974AH9-nZYchC-nqxyve-d4cqn1-Vot94w-oqAd7p-reacHf-pN74aF-e9X8Gr-roWWAp-Vtb3iX-q13JJE-8j3xrm-qc4tA4-mxPSUi-omprtS-i4EEEk-rKzLGe-TQTbpq-ejkdJi-mfQ4eZ-cAivyE-n2EDm-p1Qcqo-gNM15i-S8jsyY-jvyQyz-gYBgNG-prbC1E-hgpcdc-fMWVLT-fwKmbh-fdoGLG-NZiXrx-bQHfND-ouwqpR-cvAj4Y-iHVNN5">Toby Charlton-Taylor</a>.
+            </div>
+            <v-spacer></v-spacer>
+            <div><a class="white--text" href="https://moolah.rocks/">Moolah.rocks</a></div>
+        </v-footer>
     </v-app>
 </template>
 
 <script>
+    import {mapGetters} from 'vuex';
+    import {actions as transactionActions} from './store/transactionStore';
     import AccountList from './components/AccountList';
+    import EditTransaction from './components/transactions/EditTransaction';
     import CreateAccount from './components/CreateAccount';
     import Welcome from './components/welcome/Welcome';
     import Logout from './components/Logout';
@@ -66,12 +72,19 @@
                 showMainNav: true,
             }
         },
+        computed: {
+            showRightNavPanel() {
+                return this.selectedTransaction && this.loggedIn;
+            },
+            ...mapGetters('transactions', ['selectedTransaction']),
+        },
         store,
         components: {
             AccountList,
             CreateAccount,
+            EditTransaction,
             Welcome,
-            Logout
+            Logout,
         },
         async created() {
             const state = await client.userProfile();
--- src/components/transactions/Transactions.vue
+++ src/components/transactions/Transactions.vue
@@ -1,40 +1,33 @@
 <template>
-    <v-layout>
-        <v-flex :class="{md8: selectedTransaction}">
-            <v-list two-line subheader>
-                <v-btn
-                        fab
-                        small
-                        class="cyan accent-2"
-                        bottom
-                        right
-                        fixed
-                        @click.native.stop="addTransaction"
-                >
-                    <v-icon>add</v-icon>
-                </v-btn>
-                <v-subheader style="position:relative">
-                    {{title}}
-                </v-subheader>
-                <v-list-tile v-for="transaction in transactions" :key="transaction.id" @click.native.stop="editTransaction(transaction)">
-                    <v-list-tile-content>
-                        <v-list-tile-title>{{transaction | transactionTitle}}</v-list-tile-title>
-                    </v-list-tile-content>
-                    <v-list-tile-action>
-                        <monetary-amount :value="transaction.amount"></monetary-amount>
-                        <monetary-amount :value="transaction.balance"></monetary-amount>
-                    </v-list-tile-action>
-                </v-list-tile>
-            </v-list>
-        </v-flex>
-        <v-flex md4 v-if="selectedTransaction">
-            <edit-transaction></edit-transaction>
-        </v-flex>
-    </v-layout>
+    <v-list two-line subheader>
+        <v-btn
+                fab
+                small
+                class="cyan accent-2"
+                bottom
+                right
+                fixed
+                @click.native.stop="addTransaction"
+        >
+            <v-icon>add</v-icon>
+        </v-btn>
+        <v-subheader style="position:relative">
+            {{title}}
+        </v-subheader>
+        <v-list-tile v-for="transaction in transactions" :key="transaction.id"
+                     @click.native.stop="editTransaction(transaction)">
+            <v-list-tile-content>
+                <v-list-tile-title>{{transaction | transactionTitle}}</v-list-tile-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+                <monetary-amount :value="transaction.amount"></monetary-amount>
+                <monetary-amount :value="transaction.balance"></monetary-amount>
+            </v-list-tile-action>
+        </v-list-tile>
+    </v-list>
 </template>
 
 <script>
-    import EditTransaction from './EditTransaction';
     import {mapState, mapGetters, mapActions, mapMutations} from 'vuex';
     import client from '../../api/client';
     import MonetaryAmount from '../util/MonetaryAmount.vue';
@@ -44,8 +37,7 @@
     export default {
         props: ['accountId'],
         data() {
-            return {
-            };
+            return {};
         },
         computed: {
             title() {
@@ -53,7 +45,6 @@
             },
             ...mapState(['selectedAccountId']),
             ...mapGetters('accounts', ['accountName']),
-            ...mapGetters('transactions', ['selectedTransaction']),
             ...mapState('transactions', ['transactions']),
         },
 
@@ -93,7 +84,6 @@
 
         components: {
             MonetaryAmount,
-            EditTransaction,
         }
     };
 </script>
