a2bd929e32f9c8fbdc3dd78c55d047cead28b943
Adrian Sutton <suttona@lmax.com>
2018-01-23 18:19:05 +1000

    Improve savings goal notifications.

--- src/components/savings/SavingsGoalNotice.vue
+++ src/components/savings/SavingsGoalNotice.vue
@@ -7,26 +7,38 @@
         </v-layout>
         <v-layout row>
             <v-flex xs12>
-                <p>You have saved <monetary-amount :value="selectedAccount.balance" :omitZeroCents="true"></monetary-amount>
-                    <template v-if="selectedAccount.savingsTarget">of <monetary-amount :value="selectedAccount.savingsTarget" :omitZeroCents="true"></monetary-amount></template>
-                    <template v-if="selectedAccount.savingsStartDate">in {{daysElapsed}}.</template>
-                    <template v-if="selectedAccount.savingsEndDate">There are {{daysRemaining}} remaining to reach your goal.</template>
+                <p>You have saved
+                    <monetary-amount :value="selectedAccount.balance" :omitZeroCents="true"></monetary-amount>
+                    <template v-if="selectedAccount.savingsTarget">of your <monetary-amount :value="selectedAccount.savingsTarget" :omitZeroCents="true"></monetary-amount> target</template><!--
+                    --><template v-if="selectedAccount.savingsStartDate && !startedToday && started"> in {{daysElapsed}}</template>.
+                    <template v-if="hasReachedTarget">Congratulations!</template>
+                    <template v-else-if="!started">Savings plan starts in {{daysElapsed}}.</template>
+                    <template v-else-if="selectedAccount.savingsEndDate && started">
+                        <template v-if="dueToday">The target date is today.</template>
+                        <template v-else-if="overdue">Target end date was {{daysRemaining}} ago.</template>
+                        <template v-else>You have {{daysRemaining}} left to reach your goal.</template>
+                    </template>
                 </p>
             </v-flex>
         </v-layout>
         <v-layout row wrap>
             <v-flex v-if="hasSavingsTarget" md6 xl3>
-                <v-progress-circular :value="savingsPercent" :size="progressSize" :width="progressWidth" :rotate="180" :color="progressColor(savingsPercent)">
+                <v-progress-circular :value="savingsPercent" :size="progressSize" :width="progressWidth" :rotate="180" :color="savingsColor">
                     Saved<br>
-                    <monetary-amount :value="selectedAccount.balance" :omitZeroCents="true"></monetary-amount><br>
+                    <monetary-amount :value="selectedAccount.balance" :omitZeroCents="true"></monetary-amount>
+                    <br>
                     Target<br>
                     <monetary-amount :value="selectedAccount.savingsTarget" :omitZeroCents="true"></monetary-amount>
                 </v-progress-circular>
             </v-flex>
-            <v-flex v-if="hasTargetDates" md6 xl3>
-                <v-progress-circular :value="timePercent" :size="progressSize" :width="progressWidth" :rotate="180" :color="progressColor(savingsPercent)">
-                    {{daysElapsed}} gone<br>
-                    {{daysRemaining}} left
+            <v-flex v-if="hasTargetDates && started" md6 xl3>
+                <v-progress-circular :value="timePercent" :size="progressSize" :width="progressWidth" :rotate="180" :color="timeColor">
+                    <template v-if="notStartedYet">Start in {{daysElapsed}}<br></template>
+                    <template v-else-if="!startedToday">{{daysElapsed}} gone<br></template>
+
+                    <template v-if="dueToday">Due today</template>
+                    <template v-else-if="overdue">{{daysRemaining}} overdue</template>
+                    <template v-else>{{daysRemaining}} left</template>
                 </v-progress-circular>
             </v-flex>
         </v-layout>
@@ -38,17 +50,22 @@
     import MonetaryAmount from '../util/MonetaryAmount.vue';
     import {VProgressCircular} from 'vuetify';
     import differenceInCalendarDays from 'date-fns/difference_in_calendar_days';
-    import distanceInWordsToNow from 'date-fns/distance_in_words_to_now';
+    import distanceInWords from 'date-fns/distance_in_words';
+    import startOfToday from 'date-fns/start_of_today';
 
     export default {
         data() {
             return {
-                progressSize: 180
+                progressSize: 200,
             };
         },
         computed: {
             progressWidth() {
                 return Math.floor(this.progressSize / 10);
+            },
+
+            hasReachedTarget() {
+                return this.hasSavingsTarget && this.selectedAccount.balance >= this.selectedAccount.savingsTarget;
             },
 
             hasSavingsTarget() {
@@ -57,20 +74,49 @@
             savingsPercent() {
                 return this.selectedAccount.balance / this.selectedAccount.savingsTarget * 100;
             },
+            savingsColor() {
+                if (this.savingsPercent < 100) {
+                    return 'accent';
+                } else {
+                    return 'success';
+                }
+            },
 
             hasTargetDates() {
                 return this.hasSavings && this.selectedAccount.savingsStartDate && this.selectedAccount.savingsEndDate;
             },
+            started() {
+                return !this.notStartedYet;
+            },
+            notStartedYet() {
+                return this.selectedAccount.savingsStartDate && differenceInCalendarDays(this.selectedAccount.savingsStartDate, startOfToday()) > 0;
+            },
+            startedToday() {
+                return this.selectedAccount.savingsStartDate && differenceInCalendarDays(this.selectedAccount.savingsStartDate, startOfToday()) === 0;
+            },
             daysElapsed() {
-                return distanceInWordsToNow(this.selectedAccount.savingsStartDate);
+                return distanceInWords(startOfToday(), this.selectedAccount.savingsStartDate);
             },
             daysRemaining() {
-                return distanceInWordsToNow(this.selectedAccount.savingsEndDate);
+                return distanceInWords(startOfToday(), this.selectedAccount.savingsEndDate);
+            },
+            dueToday() {
+                return this.selectedAccount.savingsEndDate && differenceInCalendarDays(this.selectedAccount.savingsEndDate, startOfToday()) === 0;
+            },
+            overdue() {
+                return this.selectedAccount.savingsEndDate && differenceInCalendarDays(this.selectedAccount.savingsEndDate, startOfToday()) < 0;
             },
             timePercent() {
-                 const totalDays = differenceInCalendarDays(this.selectedAccount.savingsEndDate, this.selectedAccount.savingsStartDate);
-                 const elapsedDays = differenceInCalendarDays(new Date(), this.selectedAccount.savingsStartDate);
-                 return elapsedDays / totalDays * 100;
+                const totalDays = differenceInCalendarDays(this.selectedAccount.savingsEndDate, this.selectedAccount.savingsStartDate);
+                const elapsedDays = differenceInCalendarDays(new Date(), this.selectedAccount.savingsStartDate);
+                return elapsedDays / totalDays * 100;
+            },
+            timeColor() {
+                if (this.timePercent < 100) {
+                    return 'accent';
+                } else {
+                    return 'error';
+                }
             },
 
             hasSavings() {
@@ -80,15 +126,6 @@
                         this.selectedAccount.savingsEndDate);
             },
             ...mapGetters('accounts', ['accountName', 'selectedAccount']),
-        },
-        methods: {
-            progressColor(percent) {
-                if (percent < 100) {
-                    return 'accent';
-                } else {
-                    return 'green';
-                }
-            },
         },
         components: {
             VProgressCircular,
