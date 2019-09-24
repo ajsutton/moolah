5b4ec172370d470bf1e3d4c238af6a110fb39675
Adrian Sutton <adrian@symphonious.net>
2019-09-24 18:13:45 +1000

    First step towards vuetify 2.0.

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,5 @@
 module.exports = {
   presets: [
     '@vue/app'
-  ],
-  plugins: [
-    "@babel/transform-runtime",
-	["transform-imports", {
-	  "vuetify": {
-	    "transform": "vuetify/es5/components/${member}",
-		"preventFullImport": true
-	  }
-	}]
   ]
 }
