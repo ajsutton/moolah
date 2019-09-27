8e2983ce6e2863b45a237c36b1fc6d71b88f8915
Adrian Sutton <adrian@symphonious.net>
2019-09-27 15:45:53 +1000

    Add welcome panel when there is no data.

--- src/components/analysis/Analysis.vue
+++ src/components/analysis/Analysis.vue
@@ -1,6 +1,12 @@
 <template>
     <v-container fluid class="analysis">
-        <v-row>
+        <v-row v-if="!hasHistory">
+            <v-col :cols="12">
+                <getting-started></getting-started>
+            </v-col>
+        </v-row>
+
+        <v-row v-if="hasHistory">
             <v-col :cols="12">
                 <net-worth-graph></net-worth-graph>
             </v-col>
@@ -9,14 +15,15 @@
             <v-col :cols="12" :lg="6">
                 <upcoming-transactions
                     :shortTerm="true"
+                    v-if="showScheduledTransactions"
                 ></upcoming-transactions>
-                <expense-breakdown></expense-breakdown>
+                <expense-breakdown v-if="hasHistory"></expense-breakdown>
             </v-col>
-            <v-col :cols="12" :lg="6">
+            <v-col :cols="12" :lg="6" v-if="hasHistory">
                 <income-and-expense-table></income-and-expense-table>
             </v-col>
         </v-row>
-        <v-row>
+        <v-row v-if="hasHistory">
             <v-col :cols="12">
                 <categories-over-time></categories-over-time>
             </v-col>
@@ -25,25 +32,38 @@
 </template>
 
 <script>
+import { mapState } from 'vuex';
 import NetWorthGraph from './NetWorthGraph.vue';
 import ExpenseBreakdown from './ExpenseBreakdown.vue';
 import CategoriesOverTime from './CategoriesOverTime.vue';
 import UpcomingTransactions from '../transactions/UpcomingTransactions.vue';
 import IncomeAndExpenseTable from './IncomeAndExpenseTable.vue';
+import GettingStarted from './GettingStarted.vue';
 
 export default {
+    computed: {
+        bottomLayout() {
+            return this.$vuetify.breakpoint.mdAndUp ? {} : { column: true };
+        },
+        hasHistory() {
+            return this.transactions.length > 0 && this.accounts.length > 0;
+        },
+        showScheduledTransactions() {
+            return this.hasHistory || this.scheduledTransactions.length > 0;
+        },
+        ...mapState('transactions', ['transactions']),
+        ...mapState('scheduledTransactions', {
+            scheduledTransactions: 'transactions',
+        }),
+        ...mapState('accounts', ['accounts']),
+    },
     components: {
         NetWorthGraph,
         ExpenseBreakdown,
         CategoriesOverTime,
         UpcomingTransactions,
         IncomeAndExpenseTable,
-    },
-
-    computed: {
-        bottomLayout() {
-            return this.$vuetify.breakpoint.mdAndUp ? {} : { column: true };
-        },
+        GettingStarted,
     },
 };
 </script>
---
+++ src/components/analysis/GettingStarted.vue
@@ -0,0 +1,91 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col>
+                <v-card>
+                    <v-card-title>Welcome!</v-card-title>
+                    <v-card-text>
+                        <p>
+                            To get started tracking your moolah, we need to do a
+                            little setup work. Don't worry, it won't take long.
+                        </p>
+                        <div class="title">Accounts</div>
+                        <p>
+                            Accounts are the basic container of money (or debt)
+                            in Moolah. Things like your bank accounts, credit
+                            cards and loans should be created as accounts in
+                            Moolah.
+                        </p>
+                        <p class="font-weight-bold">
+                            Create an account using the
+                            <v-icon>mdi-plus</v-icon> button beside
+                            <i>Accounts</i> in the left sidebar.
+                        </p>
+                        <p>
+                            The balance of each account in Moolah should be the
+                            same as the balance of the real account it tracks.
+                            If not, you've likely missed recording a
+                            transaction.
+                        </p>
+
+                        <p>
+                            If you don't plan to add existing transactions, you
+                            can set the opening balance for each account to the
+                            real account's current balance, then just record new
+                            transactions going forward.
+                        </p>
+
+                        <div class="title">Transactions</div>
+                        <p>
+                            Once you have at least one account created, you can
+                            start tracking transactions for each payment into or
+                            out of an account. Transactions can be recorded as
+                            income, expenses or transfers between your accounts.
+                        </p>
+
+                        <p class="font-weight-bold">
+                            Create transactions from within an account view with
+                            the <v-icon>mdi-plus</v-icon> button at the top
+                            right.
+                        </p>
+
+                        <div class="title">Categories</div>
+                        <p>
+                            To allow you to understand where you're money is
+                            coming from and what it's being spent on, you can
+                            assign a category to each transaction.
+                        </p>
+                        <p>
+                            Before you can do that though, you need to create
+                            some categories.
+                        </p>
+                        <p class="font-weight-bold">
+                            Create categories from the <i>Categories</i> panel.
+                        </p>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            currentStep: 1,
+        };
+    },
+    computed: {
+        hasAccounts() {
+            return true;
+        },
+        hasCategories() {
+            return false;
+        },
+        hasTransactions() {
+            return false;
+        },
+    },
+};
+</script>
