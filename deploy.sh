220de21a12a8661e5c9a6b53f4238ff92ceec96b
Adrian Sutton <adrian@symphonious.net>
2021-01-16 06:53:57 +1000

    Try enabling deployment

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 ssh moolah@do2.symphonious.net mkdir -p moolah.rocks/versions/incoming
 rsync -r --delete-after --quiet "${TRAVIS_BUILD_DIR}/dist/" moolah@do2.symphonious.net:moolah.rocks/versions/incoming/
-ssh moolah@do2.symphonious.net ./moolah.rocks/deployMoolah.sh "${TRAVIS_COMMIT}" "${TRAVIS_BUILD_NUMBER}" "${TRAVIS_BUILD_ID}"
+ssh moolah@do2.symphonious.net ./moolah.rocks/deployMoolah.sh "${CIRCLE_SHA1}" "${CIRCLE_BUILD_NUM}" "${CIRCLE_BUILD_URL}"
