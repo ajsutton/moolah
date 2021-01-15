854c626ef37c6ab3c921e3aefd2e21bf38a6e771
Adrian Sutton <adrian@symphonious.net>
2021-01-16 08:05:37 +1000

    Use circleci env var to deploy.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 ssh moolah@do2.symphonious.net mkdir -p moolah.rocks/versions/incoming
-rsync -r --delete-after --quiet "${TRAVIS_BUILD_DIR}/dist/" moolah@do2.symphonious.net:moolah.rocks/versions/incoming/
+rsync -r --delete-after --quiet "${CIRCLE_WORKING_DIRECTORY}/dist/" moolah@do2.symphonious.net:moolah.rocks/versions/incoming/
 ssh moolah@do2.symphonious.net ./moolah.rocks/deployMoolah.sh "${CIRCLE_SHA1}" "${CIRCLE_BUILD_NUM}" "${CIRCLE_BUILD_URL}"
