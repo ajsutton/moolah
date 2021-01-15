cb2366484f344cb90cbcec565c3980ec4a50ca32
Adrian Sutton <adrian@symphonious.net>
2021-01-16 08:09:49 +1000

    Deploy the right directory.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -euo pipefail
 ssh moolah@do2.symphonious.net mkdir -p moolah.rocks/versions/incoming
-rsync -r --delete-after --quiet "${CIRCLE_WORKING_DIRECTORY}/dist/" moolah@do2.symphonious.net:moolah.rocks/versions/incoming/
+rsync -r --delete-after --quiet "~/project/dist/" moolah@do2.symphonious.net:moolah.rocks/versions/incoming/
 ssh moolah@do2.symphonious.net ./moolah.rocks/deployMoolah.sh "${CIRCLE_SHA1}" "${CIRCLE_BUILD_NUM}" "${CIRCLE_BUILD_URL}"
