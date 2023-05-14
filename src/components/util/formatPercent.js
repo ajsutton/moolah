28108817fb7cda3ecdc08615c512992c05932bab
Adrian Sutton <adrian@symphonious.net>
2023-05-14 15:44:43 +1000

    Add missing files.

---
+++ src/components/util/formatPercent.js
@@ -0,0 +1,8 @@
+export default function formatPercent(amount) {
+    return (
+        amount.toLocaleString(undefined, {
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 0,
+        }) + '%'
+    );
+}
